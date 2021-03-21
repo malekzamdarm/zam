b585ddf7f247dc8896a1f92ccc6a50642a9fcb07
malekzamdarm <50127623+malekzamdarm@users.noreply.github.com>
2021-03-21 04:33:16 +0300

    Update and rename zm.py to zmm.py

--- zmm.py
+++ zmm.py
@@ -4,16 +4,16 @@ time.sleep(2)
 print("\033[31m\n          █████▒    ▒█████     ▒██   ██▒\n        ▓██   ▒    ▒██▒  ██▒   ▒▒ █ █▒░\n        ▒████ ░   ▒██░    ██▒    ░░█░\n        ░▓█▒  ░    ▒██   ██░    ░ █ █▒\n        ░▒█░       ░ ████▓▒░   ▒██▒▒██▒\n         ▒ ░       ░ ▒░▒░▒░    ▒▒ ░ ░▓ ░\n         ░           ░ ▒ ▒░    ░░   ░▒ ░\n         ░           ░ ░ ▒      ░    ░\n                       ░ ░      ░    ░\n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \033[0;1m     Auther: Zed\n      My lion: Dark\n      Chenal Telegram: kak_zed_1\n      Note: 10$ \n      Note: ID Active a DllaKam \n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 while True:
 	h = "fox"
-	j = "fox"
+	j = "zed"
 	jj = input("  Username: ")
 	hh = input("  Passowrd: ")
 	if h == hh and j == jj :
 		print("\033[32m Afarin \033[0;1m:)")
 		print(50*"-")
 		time.sleep(1)
-		os.system("python2 0.py")
+		os.system("python2 .0.py")
 	else:
 		print("\033[33;1mHallaya !!")
 		time.sleep(1)
 		os.system("clear")
-		print("\033[31m\n
+		print("\033[31m\n          █████▒    ▒█████     ▒██   ██▒\n        ▓██   ▒    ▒██▒  ██▒   ▒▒ █ █▒░\n        ▒████ ░   ▒██░    ██▒    ░░█░\n        ░▓█▒  ░    ▒██   ██░    ░ █ █▒\n        ░▒█░       ░ ████▓▒░   ▒██▒▒██▒\n         ▒ ░       ░ ▒░▒░▒░    ▒▒ ░ ░▓ ░\n         ░           ░ ▒ ▒░    ░░   ░▒ ░\n         ░           ░ ░ ▒      ░    ░\n                       ░ ░      ░    ░\n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \033[0;1m     Auther: Zed\n      My lion: Dark\n      Chenal Telegram: kak_zed_1\n      Note: 10$\n      Note: ID Active a DllaKam \n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
