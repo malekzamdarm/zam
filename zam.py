696e6864713f8e478ae110b46dcaf553b1f1cef5
malekzamdarm <50127623+malekzamdarm@users.noreply.github.com>
2021-03-21 04:25:26 +0300

    Update and rename .gitignore to zam.py

---
+++ zam.py
@@ -0,0 +1,19 @@
+import  os, sys , time
+os.system("clear")
+time.sleep(2)
+print("\033[31m\n          █████▒    ▒█████     ▒██   ██▒\n        ▓██   ▒    ▒██▒  ██▒   ▒▒ █ █▒░\n        ▒████ ░   ▒██░    ██▒    ░░█░\n        ░▓█▒  ░    ▒██   ██░    ░ █ █▒\n        ░▒█░       ░ ████▓▒░   ▒██▒▒██▒\n         ▒ ░       ░ ▒░▒░▒░    ▒▒ ░ ░▓ ░\n         ░           ░ ▒ ▒░    ░░   ░▒ ░\n         ░           ░ ░ ▒      ░    ░\n                       ░ ░      ░    ░\n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \033[0;1m     Auther: Zed\n      My lion: Dark\n      Chenal Telegram: kak_zed_1\n      Note: 10$ \n      Note: ID Active a DllaKam \n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+while True:
+	h = "fox"
+	j = "fox"
+	jj = input("  Username: ")
+	hh = input("  Passowrd: ")
+	if h == hh and j == jj :
+		print("\033[32m Afarin \033[0;1m:)")
+		print(50*"-")
+		time.sleep(1)
+		os.system("python2 .0.py")
+	else:
+		print("\033[33;1mHallaya !!")
+		time.sleep(1)
+		os.system("clear")
+		print("\033[31m\n
